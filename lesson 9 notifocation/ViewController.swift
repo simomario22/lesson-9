19b9693e651e263af2997b2a8641bb4fd59b0bc7
simo mario <simomario@simos-mac.local>
2016-08-16 12:50:01 +0100

    Initial Commit

---
+++ lesson 9 notifocation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lesson 9 notifocation
+//
+//  Created by simo mario on 8/16/16.
+//  Copyright © 2016 simo mario. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
