fd3359642538ea7e4ec827f6d3dc696159148aee
simo mario <simomario@simos-mac.local>
2016-08-18 10:35:48 +0100

    ana  mazal daba  n nass deal video

--- lesson 9 notifocation/ViewController.swift
+++ lesson 9 notifocation/ViewController.swift
@@ -8,16 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    let ktext = "text"
 
+    @IBOutlet weak var textfield: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    // hna  kader  dakchi  libghiti  had outlit  derou  fhad  lhala darna  bli text likun  f filde kisawi text  man ba3d  kanzedou  smauay func f nsnotification  bach  lblasa  lifiha  listi9bal  ta3rafha
+    @IBAction func sendbuttontapped(sender: AnyObject) {
+        guard let text = textfield.text else {return}
+        NSNotificationCenter.defaultCenter().postNotificationName("sendbuttontapped", object: nil, userInfo: [ktext:text])
+    }
+    
+    func loadTabs () {
+        guard let ViewController = self.tabBarController?.viewControllers else {return }
+        for vc in ViewController {
+            if vc != self {
+                let _ = vc.view
+            }
+        }
+    }
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 
 
